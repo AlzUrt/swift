b9206f08f74e33dfdedbaedbe2aa8720a0be318d
alzurt <chabert.amaury2002@gmail.com>
2024-10-08 16:14:23 +0200

    first commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by CHABERT Amaury on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
